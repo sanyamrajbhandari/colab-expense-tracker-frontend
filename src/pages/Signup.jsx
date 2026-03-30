8486b26b491bec876e1d7fd287862beebeef214b
angaj shrestha <angajshr222@gmail.com>
2026-03-30 19:55:01 +0545

    changed the css of the login page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,21 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
+import "../css/Signup.css";
+import { useNavigate } from "react-router-dom";
 
-const Signup = ({ switchToLogin }) => {
+const Signup = () => {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (formData.password !== formData.confirmPassword) {
+      alert("Passwords do not match!");
+      return;
+    }
+
+    console.log(formData);
+    alert("Signup successful!");
+
+    navigate("/login");
+  };
+
   return (
     <div className="container">
-      <div className="card">
-        <h1 className="title">Create Account</h1>
-        <p className="subtitle">Sign up to get started</p>
+      <div className="card signup-card">
+        <div className="logo"></div>
 
-        <input type="text" placeholder="Full Name" className="input" />
-        <input type="email" placeholder="Email address" className="input" />
-        <input type="password" placeholder="Password" className="input" />
+        <h1 className="title">SpendWise</h1>
+        <p className="subtitle">
+          Join The Midnight Architect and design your financial future.
+        </p>
 
-        <button className="btn">Sign Up →</button>
+        <form onSubmit={handleSubmit}>
+          <label>FULL NAME</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Your Name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+
+          <label>EMAIL ADDRESS</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter your Email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+
+          <label>PASSWORD</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter Your Password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+
+          <label>CONFIRM PASSWORD</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            placeholder="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            required
+          />
+
+          <button type="submit" className="btn">
+            Sign Up
+          </button>
+        </form>
 
         <p className="footer">
           Already have an account?{" "}
-          <span onClick={switchToLogin}>Login</span>
+          <span onClick={() => navigate("/login")}>Log in</span>
         </p>
       </div>
     </div>
